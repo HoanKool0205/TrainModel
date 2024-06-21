78170e5c7007c5caa8a4817ef9efbcf325986676
HoanKool0205 <hoanlombard0205@gmail.com>
2024-06-22 01:46:57 +0700

    code

---
+++ resources/__init__.py
@@ -0,0 +1,20 @@
+from resources.i3d.pytorch_i3d import InceptionI3d
+from resources.s3d.s3d_pytorch import S3D_pytorch
+from resources.lstm.lstm_model import LSTMModel
+from resources.SwinTransformer.SwinTransformer import SwinTransformer
+
+
+def get_model(name,num_classes,fintuning, **kwargs):
+    if name == 'i3d':
+        model = InceptionI3d(400, in_channels=3)
+        model.replace_logits(num_classes)
+        model.fintuning(from_layer=fintuning)
+        return model
+    elif name == 's3d':
+        return S3D_pytorch(num_classes)
+    elif name == 'lstm':
+        return  LSTMModel(num_classes = num_classes, **kwargs)
+    elif name.split('-')[0] == 'transformer':
+        return SwinTransformer(model_name = name.split('-')[1],num_classes=num_classes)
+        
+    
---
+++ train_sh/train-j.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=I3D-RGB
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --nodes=1
+#SBATCH --time=62:30:00
+#SBATCH --mem=64gb
+#SBATCH --cpus-per-task=8
+#SBATCH --output=logs/%x_%j_%D.out
+#SBATCH --error=logs/%x_%j_%D.err
+#SBATCH --nodelist=hpc24
+
+squeue --me
+cd /work/21013187/SignLanguageRGBD/all_code
+module load python 
+module load cuda
+nvidia-smi
+python train_sh/train.py 
+
+
+
+
